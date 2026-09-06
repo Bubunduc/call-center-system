df2b6f75ae5fb71ee20e88b480fa9ae0ec735b74
Bubunduc <86464565+Bubunduc@users.noreply.github.com>
2026-09-07 01:42:12 +0300

    Add transaction for SQL insert statements
    Wrapped SQL insert statements in a transaction.

--- phone/src/phone/server/sql/sqlinitdata.sql
+++ phone/src/phone/server/sql/sqlinitdata.sql
@@ -1,17 +1,15 @@
-INSERT INTO room (name)
-VALUES
+BEGIN;
+
+INSERT INTO room (name) VALUES
     ('Главный зал'),
     ('Офис'),
     ('Переговорная');
 
-INSERT INTO device (
-    device_number,
-    room_id,
-    operator_name
-)
-VALUES
+INSERT INTO device (device_number, room_id, operator_name) VALUES
     ('100', 1, 'Вася'),
     ('101', 1, 'Коля'),
     ('202', 2, 'Юля'),
     ('203', 2, 'Анна'),
     ('301', 3, 'Иван');
+
+COMMIT;
