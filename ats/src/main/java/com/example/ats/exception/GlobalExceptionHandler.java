5f5b059a422e9f820d3214048fb3543cc811a598
Danila_Rumyancev <drumyancev@inobitec.com>
2026-09-10 12:18:40 +0300

    Рефакторинг

--- ats/src/main/java/com/example/ats/exception/GlobalExceptionHandler.java
+++ ats/src/main/java/com/example/ats/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.ats.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +13,12 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(EventValidationException.class)
 	public ResponseEntity<ErrorMessage> handleEventValidation(EventValidationException e) {
 		ErrorMessage error = new ErrorMessage(e.getMessage());
+		return ResponseEntity.badRequest().body(error);
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ErrorMessage> handleInvalidJson(HttpMessageNotReadableException e) {
+		ErrorMessage error = new ErrorMessage("Передано некорректное значение или неизвестный Enum");
 		return ResponseEntity.badRequest().body(error);
 	}
 }
