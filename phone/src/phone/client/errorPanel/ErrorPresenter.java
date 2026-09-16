020da15cdd0a57a4f61b7aa7218a551097f1ac77
Danila_Rumyancev <drumyancev@inobitec.com>
2026-09-16 16:40:56 +0300

    рефакторинг

--- phone/src/phone/client/errorPanel/ErrorPresenter.java
+++ phone/src/phone/client/errorPanel/ErrorPresenter.java
@@ -2,15 +2,16 @@ package phone.client.errorPanel;
 
 public class ErrorPresenter {
 	private final ErrorDisplay view;
-	
+
 	public ErrorPresenter(ErrorDisplay view) {
 		this.view = view;
 	}
-	
+
 	public void setErrorMessage(String message) {
 		view.setErrorMessage(message);
 	}
+
 	public void clearErrorMessage() {
-        view.setErrorMessage("");
-    }
+		view.setErrorMessage("");
+	}
 }
